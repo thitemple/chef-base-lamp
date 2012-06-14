045a2626a18fae6d3ac0d2eae6e42627d3d77b05
Thiago Temple <vintem@gmail.com>
2012-06-14 11:42:00 -0400

    mysql 5.5 specific to centos

--- cookbooks/mysql/recipes/server55centos.rb
+++ cookbooks/mysql/recipes/server55centos.rb
@@ -2,13 +2,13 @@ execute "Installing Remi repository" do
   if node['platform_version'].to_f < 6.0
     command "sudo rpm -Uvh http://dl.fedoraproject.org/pub/epel/5/i386/epel-release-5-4.noarch.rpm; sudo rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-5.rpm"
   else
-    command = "sudo rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-7.noarch.rpm; rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm"
+    command "sudo rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-7.noarch.rpm; rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm"
   end
   action :run
 end
 
 execute "Installing MySQL 5.5" do
-  command = "sudo yum --enablerepo=remi,remi-test install mysql mysql-server"
+  command "sudo yum --enablerepo=remi,remi-test install mysql mysql-server"
   action :run
 end
 
