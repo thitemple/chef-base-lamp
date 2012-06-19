648a24e82de7c9de2c1d6a1b847133e6cb79cf08
Thiago Temple <vintem@gmail.com>
2012-06-19 12:36:51 -0400

    ftp

--- cookbooks/phpmyadmin/recipes/default.rb
+++ cookbooks/phpmyadmin/recipes/default.rb
@@ -12,13 +12,13 @@ end
 if platform?(%w{centos})
 
   execute "install phpMyAdmin dependencies for php 5.3" do
-    command "sudo yum install php53-mcrypt php53-mbstring -y"
+    command "sudo yum install php53-mysqli php53-mcrypt php53-mbstring -y"
     action :run
     only_if "rpm -V php53-common"
   end
 
   execute "install phpMyAdmin dependencies for php 5.2" do
-    command "sudo yum --enablerepo=remi,remi-test install php-mcrypt php-mbstring -y"
+    command "sudo yum --enablerepo=remi,remi-test install php-mysqli php-mcrypt php-mbstring -y"
     action :run
     not_if "rpm -V php53-common"
   end
